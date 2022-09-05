fc5aaf4a0a0ba237c6e0a2e73abb8895c8a0de80
Dmitry Utkin <dutking@yandex.ru>
2022-09-05 09:42:07 +0300

    initial

---
+++ _sl_xapi/sl_xapi.js
@@ -0,0 +1,491 @@
+class Course {
+    constructor(course) {
+        this.name = course.name
+        this.iri = course.iri
+        this.slides = course.slides
+        this.lastSlideVisited = 0
+        this.timeStarted = new Date()
+        this._slidesViewed = new Set()
+        this.player = GetPlayer()
+        this.duration = 0
+    }
+
+    init() {
+        this.checkTestMode()
+
+        XAPI.getData()
+            .then((data) => (XAPI.data = data))
+            .then(() => XAPI.sendStatement(new Statement('launched', this).statement))
+            .then(() => XAPI.getState(this.iri))
+            .then((data) => {
+                if ('slidesViewed' in data) {
+                    this._slidesViewed = new Set(data.slidesViewed)
+                    this.duration = data.duration
+                }
+            })
+            .then(() => {
+                this.currentSlide = new Slide(this.slides[0])
+                this.currentSlide.interacted()
+            })
+    }
+
+    checkTestMode() {
+        if (window.location.search === '') {
+            console.log(
+                '%cCourse launched in the test mode.',
+                'font-weight:bold; color: red; font-size: 18px;'
+            );
+            this.testMode = true;
+        } else {
+            this.testMode = false;
+        }
+    }
+
+    closeCourse () {
+        console.log(
+            '%cRETURNING',
+            'color:lightblue; font-weight: bold; font-size: 18px;'
+        );
+        (function () {
+            if (window.top) {
+                return window.top;
+            }
+            return window.parent;
+        })().location = '/back/';
+        return false;
+    }
+
+    get state() {
+        return {
+            completed: this.completed,
+            slidesViewed: Array.from(this._slidesViewed),
+            duration: this.duration
+        }
+    }
+
+    get SL_slidesViewed() {
+        return Number(this.player.GetVar('slidesViewed')) 
+    }
+
+    get SL_totalSlides() {
+        return Number(this.player.GetVar('totalSlides')) + 1 //somehow in SL this var is 1 less than actual amount
+    }
+
+    get SL_completed() {
+        return this.player.GetVar('completed')
+    }
+
+    get completed() {
+        return this.slidesViewed === this.SL_totalSlides
+    }
+
+    get slidesViewed() {
+        return this._slidesViewed.size
+    }
+
+    set slidesViewed(v){
+        this._slidesViewed.add(v)
+        this.state.slidesViewed = Array.from(this._slidesViewed)
+    }
+
+    updateSlidesViewed() {
+        let slideNumber = isNaN(this.player.GetVar('slideNumber')) ? 0 : this.player.GetVar('slideNumber')
+        this.slidesViewed = slideNumber
+        if(slideNumber !== 0) {
+
+            this.proceedSlides(slideNumber)
+        }
+   }
+
+    proceedSlides(slideNumber){
+        this.currentSlide.exited()
+        this.lastSlideVisited = slideNumber
+        this.currentSlide = new Slide(this.slides.filter(s => s.index === slideNumber)[0])
+        this.currentSlide.interacted()
+    }
+
+    setMenu(){
+        Array.from(this._slidesViewed).forEach(slide => this.player.SetVar('slideVisited', slide))
+    }
+
+    exitCourse() {
+        console.log(this.currentSlide)
+        this.currentSlide.exited()
+        this.duration = this.duration + (new Date() - this.timeStarted)
+        XAPI.postState(this.iri, this.state)
+        .then(() => {
+            if (this.SL_completed || this.completed) {
+                XAPI.sendStatement(
+                    new Statement('completed', this).statement
+                ).then(() => {
+                    if (this.slidesViewed >= this.SL_totalSlides) {
+                        return XAPI.sendStatement(
+                            new Statement('passed', this).statement
+                        )
+                    } else {
+                        return XAPI.sendStatement(
+                            new Statement('failed', this).statement
+                        )
+                    }
+                }).then(() => XAPI.sendStatement(
+                    new Statement('exited', this).statement
+                )).then(() => this.closeCourse())
+            } else {
+                XAPI.sendStatement(
+                    new Statement('exited', this).statement
+                ).then(() => this.closeCourse())
+            }
+        })
+        
+    }
+}
+
+class Slide {
+    constructor(slide) {
+        this.index = slide.index
+        this.name = slide.name
+        this.iri = slide.iri
+        this.timeStarted = new Date()
+    }
+
+    interacted() {
+        XAPI.sendStatement(new Statement('interacted', this).statement)
+    }
+
+    exited() {
+        XAPI.sendStatement(new Statement('exited', this).statement)
+    }
+}
+
+class XAPI {
+    constructor() {}
+
+    static async getData() {
+        if (!window.course.testMode) {
+            if (
+                window.location.search.includes('xAPILaunchService') &&
+                window.location.search.includes('xAPILaunchKey')
+            ) {
+                console.log(
+                    '%cxAPI Launch found',
+                    'color:lightblue;font-size:16px;font-weight: bold;'
+                );
+
+                let queryParams = XAPI.parseQuery(window.location.search);
+                const response = await fetch(
+                    queryParams.xAPILaunchService +
+                    'launch/' +
+                    queryParams.xAPILaunchKey, {
+                        method: 'POST',
+                    }
+                );
+
+                return await response.json();
+            } else {
+                let queryParams = XAPI.parseQuery(window.location.search);
+                let context = {};
+                if (queryParams.context) {
+                    context = JSON.parse(queryParams.context);
+                }
+                let data = {
+                    endpoint: queryParams.endpoint,
+                    auth: queryParams.auth,
+                    actor: JSON.parse(queryParams.actor),
+                    registration: queryParams.registration,
+                    context: context,
+                };
+
+                if (Array.isArray(data.actor.account)) {
+                    data.actor.account = data.actor.account[0];
+                }
+
+                if (Array.isArray(data.actor.name)) {
+                    data.actor.name = data.actor.name[0];
+                }
+
+                if (
+                    data.actor.account &&
+                    data.actor.account.accountServiceHomePage
+                ) {
+                    data.actor.account.homePage =
+                        data.actor.account.accountServiceHomePage;
+                    data.actor.account.name = data.actor.account.accountName;
+                    delete data.actor.account.accountServiceHomePage;
+                    delete data.actor.account.accountName;
+                }
+
+                return new Promise((resolve, reject) => resolve(data));
+            }
+        } else {
+            return new Promise((resolve, reject) =>
+                resolve({
+                    actor: 'Unknown',
+                })
+            );
+        }
+    }
+
+    static parseQuery(queryString) {
+        let query = {};
+        let pairs = (
+            queryString[0] === '?' ? queryString.substr(1) : queryString
+        ).split('&');
+
+        pairs.forEach((pair) => {
+            let [key, value] = pair.split('=');
+            query[decodeURIComponent(key)] = decodeURIComponent(value || '');
+        });
+
+        return query;
+    }
+
+    static async sendStatement(stmt) {
+        console.log(`%c...sending statement: ${stmt.verb.display["en-US"]}`, 'color:gray;');
+        console.log(stmt);
+        if (!window.course.testMode) {
+            const response = await fetch(XAPI.data.endpoint + 'statements', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    'Authorization': XAPI.data.auth,
+                    'X-Experience-API-Version': '1.0.3',
+                },
+                body: JSON.stringify(stmt),
+            });
+
+            const result = await response.json();
+            return Promise.resolve(result)
+        }
+    }
+
+    static getURL(stateId) {
+        let agentObj = {
+            objectType: 'Agent',
+            name: XAPI.data.actor.name,
+            account: {
+                name: XAPI.data.actor.account.name,
+                homePage: XAPI.data.actor.account.homePage,
+            },
+        };
+
+        let agent = encodeURIComponent(JSON.stringify(agentObj).slice(1, -1));
+
+        let activityId = window.course.iri
+
+        let str = `activityId=${activityId}&stateId=${stateId}&agent={${agent}}`;
+
+        let url = `${XAPI.data.endpoint}activities/state?${str}`;
+
+        return url;
+    }
+
+    static async getState(stateId) {
+        console.log(`%c...getting state for: ${stateId}`, 'color:gray;');
+
+        try {
+            let url = XAPI.getURL(stateId);
+
+            let res = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Authorization': XAPI.data.auth,
+                    'X-Experience-API-Version': '1.0.3',
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+            });
+
+            let data = await res.json();
+            console.log(data)
+            return data;
+        } catch {
+            console.warn(`%cState for ${stateId} was not found!`, 'color:red;')
+            return Promise.resolve({
+                id: stateId,
+                result: false
+            })
+        }
+    }
+
+    static async postState(stateId, stateObj) {
+        if (!window.course.testMode) {
+            let url = XAPI.getURL(stateId);
+            let res = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    Authorization: XAPI.data.auth,
+                    'X-Experience-API-Version': '1.0.3',
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+                body: JSON.stringify(stateObj),
+            });
+            console.log(`%cState posted: ${res.ok}`, 'color:gray;');
+            return Promise.resolve(res.ok)
+        }
+    }
+
+    static async deleteState(stateId) {
+        if (!window.course.testMode) {
+            let url = XAPI.getURL(stateId);
+            let res = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: XAPI.data.auth,
+                    'X-Experience-API-Version': '1.0.3',
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+            });
+            console.log(`%c${stateId} state deleted: ${res.ok}`, 'color:gray;');
+            return Promise.resolve(res.ok)
+        }
+    }
+}
+
+class Statement {
+    constructor(verb, item) {
+        this.item = item
+        this.iri = item.iri
+        this.name = item.name
+        this.verbString = verb;
+        this.time = new Date();
+        this.prevDuration = (this.item instanceof Course) ? this.item.duration : 0
+    }
+
+    get id() {
+        return {
+            id: window.crypto.randomUUID()
+        };
+    }
+
+    get object() {
+        let object = {
+            object: {
+                id: '',
+                definition: {},
+            },
+        };
+
+
+        object.object.id = this.iri;
+
+        object.object.definition = {
+            name: {
+                'en-US': this.name,
+                'ru-RU': this.name,
+            },
+            description: {
+                'en-US': this.name,
+                'ru-RU': this.name,
+            },
+
+        }
+
+        return object;
+    }
+
+    get context() {
+        let object = {
+            context: {
+                registration: XAPI.data.registration,
+                contextActivities: {
+                    grouping: XAPI.data ?.context?.contextActivities?.grouping || [],
+                },
+            },
+        };
+
+        return object;
+    }
+
+    get verb() {
+        return {
+            verb: verbs[this.verbString],
+        };
+    }
+
+    get actor() {
+        return {
+            actor: XAPI.data.actor,
+        };
+    }
+
+    get result() {
+        let object = {
+            result: {},
+        };
+
+        if (this.verbString === 'completed') {
+            Object.assign(object.result, {
+                completion: true,
+                duration: moment
+                    .duration(
+                        Math.round(((this.time - this.item.timeStarted) + this.prevDuration) / 1000),
+                        'seconds'
+                    )
+                    .toISOString(),
+            });
+        }
+
+        if (this.verbString === 'passed' || this.verbString === 'failed') {
+            Object.assign(object.result, {
+                success: this.verbString === 'passed' ? true : false,
+                score: {
+                    raw: this.item.slidesViewed,
+                    scaled: (1 / this.item.SL_totalSlides) * this.item.slidesViewed,
+                    min: 0,
+                    max: this.item.SL_totalSlides,
+                },
+                duration: moment
+                    .duration(
+                        Math.round(((this.time - this.item.timeStarted) + this.prevDuration) / 1000),
+                        'seconds'
+                    )
+                    .toISOString(),
+            });
+        }
+
+        if (this.verbString === 'exited') {
+            Object.assign(object.result, {
+                duration: moment
+                    .duration(
+                        Math.round(((this.time - this.item.timeStarted) + this.prevDuration) / 1000),
+                        'seconds'
+                    )
+                    .toISOString(),
+            });
+        }
+
+        return object;
+    }
+
+    get timestamp() {
+        return {
+            timestamp: this.time
+        };
+    }
+
+    get statement() {
+        let finalStatement = Object.assign({},
+            this.id,
+            this.actor,
+            this.verb,
+            this.object,
+            this.context,
+            this.timestamp,
+            this.result
+        );
+
+        if (
+            this.verbString === 'interacted' ||
+            this.verbString === 'launched'
+        ) {
+            delete finalStatement.result;
+        }
+
+        return finalStatement;
+    }
+}
+
+window.addEventListener('load', async function () {
+    window.XAPI = XAPI;
+    window.course = new Course(course)
+    window.course.init()
+});
---
+++ index.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const fs = require('fs-extra');
+const HTMLParser = require('node-html-parser');
+const AdmZip = require('adm-zip');
+const { DOMParser, XMLSerializer } = require('@xmldom/xmldom')
+
+async function init () {
+    let date = new Date();
+    let mm = Number(date.getMinutes()).toLocaleString({ minimumIntegerDigits: 2 });
+    let hh = Number(date.getHours()).toLocaleString({ minimumIntegerDigits: 2 });
+    let day = Number(date.getDate()).toLocaleString({ minimumIntegerDigits: 2 });
+    let month = (Number(date.getMonth()) + 1).toLocaleString({
+        minimumIntegerDigits: 2,
+    });
+    let year = Number(date.getFullYear());
+    let folderDate = `${year}_${month}_${day}_${hh}_${mm}`;
+
+    const course = {
+        name: '',
+        iri: '',
+        slides: []
+    }
+
+    let files = await fs.readdir('_source')
+    files.forEach(f => {
+        if(fs.lstatSync(path.join('_source', f)).isDirectory() && fs.existsSync(path.join('_source', f, 'story.html'))){
+            // parsing tincan.xml to get course data and to modify 
+            const parser = new DOMParser()
+            let dom = parser.parseFromString(fs.readFileSync(path.join('_source', f, 'tincan.xml'), 'utf-8'), 'text/xml')
+
+            //getting course.id
+            course.iri = dom.getElementsByTagName('activity')[0].getAttribute('id')
+            /* dom.getElementsByTagName('activity')[0].setAttribute('id', course.iri) */
+
+            //getting course.name
+            course.name = dom.getElementsByTagName('description')[0].textContent
+            dom.getElementsByTagName('name')[0].textContent = course.name
+
+            //changing launch tag
+            dom.getElementsByTagName('launch')[0].textContent = 'story.html'
+
+            //changing slide activities IDs and setting course.slides
+            Array.from(dom.getElementsByTagName('activity')).slice(1).forEach((a, index) => {
+                a.setAttribute('id', `${course.iri}/slide_${index}`)
+                course.slides.push({
+                    index, 
+                    iri: `${course.iri}/slide_${index}`,
+                    name: a.getElementsByTagName('name')[0].textContent
+                })
+            })
+
+            fs.outputFileSync(
+                path.join('_source', f, 'tincan.xml'),
+                new XMLSerializer().serializeToString(dom),
+                'utf-8'
+            )
+
+            //adding scripts to story.html
+            let storyHTML = fs.readFileSync(path.join('_source', f, 'story.html'), 'utf-8')
+            let html = HTMLParser.parse(storyHTML, {
+                lowerCaseTagName: false,
+                comment: false,
+            });
+
+            let title = html.querySelector('title');
+            title.set_content(course.name);
+
+            let head = html.querySelector('head');
+            let headData = head.innerHTML;
+            let headScripts = `
+                <script>
+                    const course = {
+                        name: "${course.name}",
+                        iri: "${course.iri}",
+                        slides: [${course.slides.map(s => {
+                            return `{index: ${s.index},
+                                iri: "${s.iri}",
+                                name: "${s.name}"}`
+                        }).join(',')}]}
+                </script>
+                <script src="_sl_xapi/verbs.js" defer></script>
+                <script src="_sl_xapi/moment.js" defer></script>
+                <script src="_sl_xapi/sl_xapi.js" type="module" defer></script>
+            `
+            head.set_content(headData + headScripts);
+            fs.outputFileSync(
+                path.join('_source', f, 'story.html'),
+                html.toString(),
+                'utf-8'
+            )
+
+            //create structure.json
+            let structure = `
+            {
+                "id": "${course.iri}",
+                "type": "course",
+                "component": "course",
+                "name": "${course.name}",
+                "version": 1,
+                "items": [${course.slides.map((s, index) => {
+                    return `
+                    {
+                        "id": "${s.iri}",
+                        "parent": "${course.iri}",
+                        "name": "${s.name}",
+                        "type": "slide",
+                        "type_name": "Слайд",
+                        "weight": 1,
+                        "is_leaf": true,
+                        "order": ${index}
+                    }
+                    `
+                }).join(',')}]
+            }
+            `
+
+            fs.outputFileSync(
+                path.join('_source', f, 'structure.json'),
+                structure,
+                'utf-8'
+            )
+
+            //deleting files
+            fs.rmSync(path.join('_source', f, 'lms'), { recursive: true, force: true });
+            fs.unlinkSync(path.join('_source', f, 'analytics-frame.html'))
+            fs.unlinkSync(path.join('_source', f, 'index_lms.html'))
+            fs.copySync('_sl_xapi', path.join('_source', f, '_sl_xapi'))
+
+            //creating zip
+            let zipName = f.includes(' - Storyline output') ? f.replace(' - Storyline output', '') : f
+            let zip = new AdmZip();
+            zip.addLocalFolder(path.join('_source', f));
+            zip.writeZip(path.join('_source', 'zip', `${zipName}_${folderDate}.zip`));
+            
+        }
+    })
+}
+
+init()
